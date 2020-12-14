927e0c9305eea5c698851eeb65c648b19d06b5cd
Scarlett Wang <scarlett@users.noreply.github.com>
2020-12-14 18:23:08 -0500

    download July 1 data
    https://github.com/scarlett/coronavirus-data/tree/41f823d90887251735ad4e6f4814548fc9526606

---
+++ pulling data_by_modzcta_01JUL20.R
@@ -0,0 +1,2 @@
+a<-read.csv(file="https://raw.githubusercontent.com/nychealth/coronavirus-data/41f823d90887251735ad4e6f4814548fc9526606/data-by-modzcta.csv", stringsAsFactors = FALSE)
+write.csv(a, "C:/Users/scarlett/Documents/Data/corona risk pop/coronavirus-data/data_by_modzcta_01JUL20.csv")
